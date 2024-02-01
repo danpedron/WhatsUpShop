40b9f27a193a334498e9e4e4c7aeeea29f7e3852
Helizelton Marcelo Pedron <danpedron@gmail.com>
2024-02-01 11:20:28 -0300

    Alterado dbcon para não expor dados de login no banco de dados. Essas informações agora estão no arquivo secretInfo.php.

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,7 @@
 <?php
+require_once("secretInfo.php");
 $host = "localhost";
 $tableprefix = "wappstore_"; //You can change this table prefix, don't use white spaces, use underscores instead for example this_is_my_table_prefix_
 $databasename = "wappstore";
-$dbuser = "wappstore_user";
-$dbpassword = "123Mudar";
+$dbuser = DB_USERNAME;
+$dbpassword = DB_PASSWORD;
